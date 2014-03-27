ca7539bf8861c9cbf49d6d8d598825ebdd4e4e3e
Derek Passen <dpassen1@gmail.com>
2014-03-26 22:03:12 -0400

    Prep for initial release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject intransit "0.1.0-SNAPSHOT"
+(defproject intransit "0.1.0"
   :description "Clojure library for retrieving data from the CTA API"
   :url "https://github.com/dpassen1/intransit"
   :license {:name "MIT"
