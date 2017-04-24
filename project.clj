23c161ca6499e20faa188bfaca8968c1be4324bb
Derek Passen <dpassen1@gmail.com>
2017-04-24 16:17:11 -0500

    Update to latest clj-http (3.5.0)

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :dependencies [[org.clojure/clojure   "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http              "3.4.1"]
+                 [clj-http              "3.5.0"]
                  [clojure.java-time     "0.2.2"]])
