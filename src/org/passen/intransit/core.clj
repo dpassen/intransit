af98bb8d3d9ce8fc3967affc967673d4cce5ac63
Derek Passen <dpassen1@gmail.com>
2025-12-30 17:28:08 -0600

    Use more vertical real estate

--- src/org/passen/intransit/core.clj
+++ src/org/passen/intransit/core.clj
@@ -36,15 +36,16 @@
   [arrival]
   (-> arrival
       (select-keys [:staNm :arrT :rt :destNm :rn :isDly :isFlt :isApp :isSch])
-      (set/rename-keys {:staNm  :station
-                        :arrT   :arrival-time
-                        :rt     :route
-                        :destNm :destination
-                        :rn     :run-number
-                        :isDly  :delayed?
-                        :isFlt  :fault?
-                        :isApp  :approaching?
-                        :isSch  :scheduled?})
+      (set/rename-keys
+       {:staNm  :station
+        :arrT   :arrival-time
+        :rt     :route
+        :destNm :destination
+        :rn     :run-number
+        :isDly  :delayed?
+        :isFlt  :fault?
+        :isApp  :approaching?
+        :isSch  :scheduled?})
       (update :arrival-time parse-cta-timestamp)
       (update :run-number parse-long)
       (update :route keyword)
@@ -58,13 +59,15 @@
   for all platforms at a given train station"
   [api-key & {:keys [station-id stop-id route max-results]}]
   (let [url "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx"]
-    (-> (http/get url {:query-params
-                       {:key        api-key
-                        :mapid      station-id
-                        :stpid      stop-id
-                        :rt         (cond-> route (some? route) name)
-                        :max        max-results
-                        :outputType output-type}})
+    (-> url
+        (http/get
+         {:query-params
+          {:key        api-key
+           :mapid      station-id
+           :stpid      stop-id
+           :rt         (cond-> route (some? route) (name))
+           :max        max-results
+           :outputType output-type}})
         (parse-common)
         (set/rename-keys {:eta :arrivals})
         (update :arrivals (partial map handle-arrival)))))
@@ -73,9 +76,10 @@
   [follow]
   (-> follow
       (select-keys [:staNm :arrT :destNm])
-      (set/rename-keys {:staNm  :station
-                        :arrT   :arrival-time
-                        :destNm :destination})
+      (set/rename-keys
+       {:staNm  :station
+        :arrT   :arrival-time
+        :destNm :destination})
       (update :arrival-time parse-cta-timestamp)))
 
 (defn follow
@@ -83,10 +87,12 @@
   at all subsequent stations for which that train is estimated to arrive"
   [api-key & {:keys [run-number]}]
   (let [url "http://lapi.transitchicago.com/api/1.0/ttfollow.aspx"]
-    (-> (http/get url {:query-params
-                       {:key        api-key
-                        :runnumber  run-number
-                        :outputType output-type}})
+    (-> url
+        (http/get
+         {:query-params
+          {:key        api-key
+           :runnumber  run-number
+           :outputType output-type}})
         (parse-common)
         (dissoc :position)
         (set/rename-keys {:eta :follows})
@@ -96,10 +102,11 @@
   [position]
   (-> position
       (select-keys [:nextStaNm :arrT :destNm :rn])
-      (set/rename-keys {:nextStaNm :next-station
-                        :arrT      :arrival-time
-                        :destNm    :destination
-                        :rn        :run-number})
+      (set/rename-keys
+       {:nextStaNm :next-station
+        :arrT      :arrival-time
+        :destNm    :destination
+        :rn        :run-number})
       (update :arrival-time parse-cta-timestamp)
       (update :run-number parse-long)))
 
@@ -107,7 +114,7 @@
   [{:keys [train] :as route}]
   (let [route-name (keyword "@name")
         color      (-> route (get route-name) (str/capitalize) (keyword))
-        positions  (cond-> train (map? train) list)]
+        positions  (cond-> train (map? train) (list))]
     [color (map handle-position positions)]))
 
 (defn positions
@@ -115,10 +122,12 @@
   and basic info and their locations for one or more specified routes"
   [api-key routes]
   (let [url "http://lapi.transitchicago.com/api/1.0/ttpositions.aspx"]
-    (-> (http/get url {:query-params
-                       {:key        api-key
-                        :rt         (map name routes)
-                        :outputType output-type}})
+    (-> url
+        (http/get
+         {:query-params
+          {:key        api-key
+           :rt         (map name routes)
+           :outputType output-type}})
         (parse-common)
         (set/rename-keys {:route :routes})
         (update :routes (partial into {} (map handle-route))))))
